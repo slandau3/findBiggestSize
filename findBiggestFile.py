31b5fc274bd4436dbbd50685b288843a25c38a6f
Steven Landau <skl8881@g.rit.edu>
2016-05-14 14:35:29 -0400

    cleaned it up a little. added a main

--- findBiggestFile.py
+++ findBiggestFile.py
@@ -1,9 +1,10 @@
 #! A program to find the largest specific files inside a general directory.
 # findBiggestFile.py
-# Created by dka
+# Created by Slandau3
 
 import os
 import time
+
 
 sizedic = {}
 def getSize(path):
@@ -17,20 +18,25 @@ def getSize(path):
         except Exception as e:
             pass
 
-startTime = time.time()
-getSize(r'C:\Users') # change this to any general sub directory of a drive. It should be C:\'Something'
-endTime = time.time()
 
-sizeList = (sorted(sizedic, reverse=True)) # gets a list of largest sizes (max -> min)
+def main():
+    startTime = time.time()
+    getSize(r'C:\Users')  # change this to any general sub directory of a drive. It should be C:\'Something'
+    endTime = time.time()
 
-i = 0
-for x in sizeList:
-    print('\n')
-    print(sizedic[x])
-    print(x/1073741824)
+    sizeList = (sorted(sizedic, reverse=True))  # gets a list of largest sizes (max -> min)
 
-    i += 1
-    if (i == 10):
-        break
+    i = 0
+    for x in sizeList:
+        i+=1
+        print('\n')
+        print(str(i) + '.', sizedic[x])
+        print(x / 1073741824)
 
-print('\nTime to search: %s seconds' % (round(endTime-startTime, 2)))
+        if (i == 10):
+            break
+
+    print('\nTime to search: %s seconds' % (round(endTime - startTime, 2)))
+
+if __name__ == '__main__':
+    main()
